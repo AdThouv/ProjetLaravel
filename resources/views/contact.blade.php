d272997eac258023d66c8d8220f1edb90fe6c8b5
Adrien Thouvenin <adaura51@gmail.com>
2021-02-09 12:10:12 +0100

    feat : first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+<h1>Contact</h1>
+<footer>à propos</footer>
+</body>
+</html>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<header><h1> Bienvenue sur le Projet Laravel </h1></header>
+    @yield('contenu')
+<footer>à propos</footer>
+</body>
+</html>
